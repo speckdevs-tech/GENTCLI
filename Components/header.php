f899c2da0ed04ea1663eefb61bdfb5fb96a003d1
Speckdevs Tech <speckdevs@gmail.com>
2026-02-23 22:28:17 +0530

    update files

--- Components/header.php
+++ Components/header.php
@@ -1,3 +1,20 @@
 <nav>
-     
+    <div class="left">
+        <a href="">
+            <h1 class="heading">GENTCLI</h1>
+        </a>
+    </div>
+    <div class="middle">
+        <ul>
+            <li><a href="" class="paragraph ">Shop</a></li>
+            <li><a href="" class="paragraph">New Arrivals</a></li>
+            <li><a href="" class="paragraph">About</a></li>
+        </ul>
+    </div>
+    <div class="right">
+        <ul>
+            <li><a href=""><ion-icon name="search-outline"></ion-icon></a></li>
+            <li><a href=""><ion-icon name="person-outline"></ion-icon></a></li>
+            <li><a href=""><ion-icon name="cart-outline"></ion-icon></a></li>
+        </ul>
 </nav>
--- index.php
+++ index.php
@@ -5,8 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gentcli | Home</title>
     <?php include 'Components/links.php'; ?>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-    
+    <?php include 'Components/header.php'; ?>
+    <section class="hero"></section>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
