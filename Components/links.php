28e51e37666567f05c13db2ddf0396a193eb5e55
Speckdevs Tech <speckdevs@gmail.com>
2026-02-23 21:45:40 +0530

    New Files Created

---
+++ Components/links.php
@@ -0,0 +1,7 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+<!-- Font Links End Here -->
+<!-- CSS Link -->
+<link rel="stylesheet" href="./styles/style.css">
