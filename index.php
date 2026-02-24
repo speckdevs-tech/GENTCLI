92100c6de9651594d3211760aeb8c73811a864ba
Speckdevs Tech <speckdevs@gmail.com>
2026-02-24 21:37:41 +0530

    update File

--- index.php
+++ index.php
@@ -9,7 +9,9 @@
 </head>
 <body>
     <?php include 'Components/header.php'; ?>
-    <section class="hero"></section>
+    <section class="hero">
+        <img src="https://id-preview--36861af3-7c5d-439a-9e02-15259b26db5c.lovable.app/assets/hero-image-CJYYPZOb.jpg" alt="">
+    </section>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
