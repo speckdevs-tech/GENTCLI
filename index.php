b82ee063f7db732bd06c06ed656362fa92047d1a
Speckdevs Tech <speckdevs@gmail.com>
2026-02-26 22:30:32 +0530

    hero section Developed

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
 <body>
     <?php include 'Components/header.php'; ?>
     <section class="hero">
+        <div class="overlay"></div>
+        <div class="h-main">
+            <h2 class="paragraph">Spring / Summer 2025</h2>
+            <h1 class="heading">Timeless<br>Elegance</h1>
+            <p class="paragraph">Discover our carefully curated collection of <br>contemporary essentials, designed for the modern <br>individual who values quality and style.</p>
+            <div class="btns">
+                <button class="paragraph">Explore Collection</button>
+                <button class="paragraph">Our Story</button>
+            </div>
+        </div>
         <img src="images/hero.png" alt="Hero Image">
     </section>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
