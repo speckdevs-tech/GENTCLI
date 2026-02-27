531eae5b380aadce2a04b630008a6ca4d9d5e1a9
Speckdevs Tech <speckdevs@gmail.com>
2026-02-27 22:50:16 +0530

    New arrivals section

--- Components/links.php
+++ Components/links.php
@@ -1,6 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=GFS+Didot&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Playfair:ital,opsz,wght@0,5..1200,300..900;1,5..1200,300..900&display=swap" rel="stylesheet">
 
 <!-- Font Links End Here -->
 <!-- CSS Link -->
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <?php include 'Components/links.php'; ?>
     <link rel="stylesheet" href="styles/style.css">
 </head>
+
 <body>
     <?php include 'Components/header.php'; ?>
     <section class="hero">
@@ -14,7 +16,8 @@
         <div class="h-main">
             <h2 class="paragraph">Spring / Summer 2025</h2>
             <h1 class="heading">Timeless<br>Elegance</h1>
-            <p class="paragraph">Discover our carefully curated collection of <br>contemporary essentials, designed for the modern <br>individual who values quality and style.</p>
+            <p class="paragraph">Discover our carefully curated collection of <br>contemporary essentials, designed for
+                the modern <br>individual who values quality and style.</p>
             <div class="btns">
                 <button class="paragraph">Explore Collection</button>
                 <button class="paragraph">Our Story</button>
@@ -22,7 +25,62 @@
         </div>
         <img src="images/hero.png" alt="Hero Image">
     </section>
+    <!-- end of hero section -->
+    <section class="newarrivals">
+        <h2 class="paragraph">Featured</h2>
+        <div class="row">
+            <h1 class="heading">New Arrivals</h1>
+            <button>View All <ion-icon name="arrow-forward-outline"></ion-icon></button>
+        </div>
+        <div class="products">
+            <a href="">
+
+                <div class="product">
+                    <div class="imgbox">
+                        <img src="images/product-1.jpg" alt="Product 1">
+                    </div>
+                    <h3 class="heading">Classic Blazer</h3>
+                    <p class="category paragraph">Top</p>
+                    <p class="paragraph">$199.99</p>
+                </div>
+            </a>
+            <a href="">
+
+                <div class="product">
+                    <div class="imgbox">
+                        <img src="images/product-2.jpg" alt="Product 2">
+                    </div>
+                    <h3 class="heading">Tailored Trousers</h3>
+                    <p class="category paragraph">Top</p>
+                    <p class="paragraph">$149.99</p>
+                </div>
+            </a>
+            <a href="">
+
+                <div class="product">
+                    <div class="imgbox">
+                        <img src="images/product-3.jpg" alt="Product 1">
+                    </div>
+                    <h3 class="heading">Classic Blazer</h3>
+                    <p class="category paragraph">Top</p>
+                    <p class="paragraph">$199.99</p>
+                </div>
+            </a>
+            <a href="">
+
+                <div class="product">
+                    <div class="imgbox">
+                        <img src="images/product-4.jpg" alt="Product 2">
+                    </div>
+                    <h3 class="heading">Tailored Trousers</h3>
+                    <p class="category paragraph">Top</p>
+                    <p class="paragraph">$149.99</p>
+                </div>
+            </a>
+        </div>
+    </section>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
+
 </html>
